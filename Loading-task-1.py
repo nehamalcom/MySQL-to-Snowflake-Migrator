368e0721fa8e1c5b268dfd2b3af577ad6cfa1618
nehamalcom <nehamalcom@gmail.com>
2021-06-24 19:23:00 +0530

    Initial version of completely working data loader from MySQL to Snowflake, used sqlalchemy and follows to_sql method

--- Loading-task-1.py
+++ Loading-task-1.py
@@ -35,16 +35,22 @@ def CreateSnowflakeDBandSchema (
           user=sqUser,
           password=sqPswd,
           host=sqHost,
-          port="1983"
+          port="1983",
+          database="qmscore"
       )
     sqq = sqConnection.cursor(buffered=True)
     print("Connected to MySQL")
 
     # <--------------------Command Execution-------------------->
+    import pandas as pd
+    from snowflake.connector.pandas_tools import write_pandas
     # Getting table company_dim
     sqq.execute("USE qmscore")
     sqq.execute("SELECT * FROM company_dim LIMIT 1000")
-    #output = sqq.fetchall()
+    table_rows = sqq.fetchall()
+    df = pd.DataFrame(table_rows)
+    print("Obtained rows from MySQL and converted into Pandas dataframe")
+    print(df)
     #print(output)
     #import csv
     #with open("out.csv", "w", newline='') as csv_file: 
@@ -80,12 +86,51 @@ def CreateSnowflakeDBandSchema (
     print("Connected to Snowflake")
 
     # <--------------------Command Execution-------------------->
+    from snowflake.sqlalchemy import URL
+    from sqlalchemy import create_engine
     sfq.execute("USE ROLE DEMO_ACCESS")
     sfq.execute("USE WAREHOUSE COMPUTE_WH")
     sfq.execute("USE DATABASE DEMO_DB")
     sfq.execute("USE SCHEMA DEMO_DB.TESTANALYTICS")
-    sfq.execute("CREATE OR REPLACE TABLE abc_table(name varchar(20), age integer)")
-
+    query = """CREATE OR REPLACE TABLE company_dim (
+  company_seq_id smallint NOT NULL,
+  company_name varchar(250) DEFAULT NULL,
+  company_cik int DEFAULT NULL,
+  company_primary_symbol varchar(20) DEFAULT NULL,
+  company_short_name varchar(100) DEFAULT NULL,
+  company_long_name varchar(250) DEFAULT NULL,
+  company_category_code varchar(5) DEFAULT NULL,
+  company_category varchar(50) DEFAULT NULL,
+  country_seq_id smallint DEFAULT NULL,
+  company_ind_class_system varchar(200) DEFAULT NULL,
+  company_sic varchar(300) DEFAULT NULL,
+  company_sector_type varchar(20) DEFAULT NULL,
+  company_sector varchar(50) DEFAULT NULL,
+  company_industry varchar(100) DEFAULT NULL,
+  company_sub_industry varchar(100) DEFAULT NULL,
+  last_fiscal_year_end date DEFAULT NULL,
+  fiscal_adjustment tinyint DEFAULT NULL,
+  quarter_adjustment tinyint DEFAULT NULL,
+  reporting_currency varchar DEFAULT NULL,
+  PRIMARY KEY (company_seq_id) )
+    """
+    #sfq.execute(query)
+    #success, nchunks, nrows, _ = write_pandas(conn=sfConnection,df=df,table_name='company_dim')
+    engine = create_engine(URL(
+        account = sfAccount,
+        user = sfUser,
+        password = sfPswd,
+        database = sfDatabase,
+        schema = sfSchema,
+        warehouse = 'COMPUTE_WH',
+        role='DEMO_ACCESS',
+    ))
+    engineConnection = engine.connect()
+    print("Engine created")
+    df.to_sql('company_dim', con=engine, index=False)
+    print("Dataframe loaded into Snowflake")
+    engineConnection.close()
+    engine.dispose()
     print('Steps complete')
     sqq.close()
     sqConnection.close()
