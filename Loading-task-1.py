cb3b82e31ba54270362cc5bd6b597db4b468a5e5
nehamalcom <nehamalcom@gmail.com>
2021-06-25 14:17:23 +0530

    Modularised the earlier version

--- Loading-task-1.py
+++ Loading-task-1.py
@@ -2,15 +2,11 @@
 # For first 1000 companies
 # Load company_dim, security_dim and then clean_financial_fact
 
-def CreateSnowflakeDBandSchema (
-    sfPswd = '',
+def mysqlConnector (
     sqPswd = '',
-    sfAccount = 'myAccount.my-region',
-    sfUser = 'my_user',
-    sfDatabase = 'DEMO_DB',
-    sfSchema = 'DEMO_SCHEMA',
     sqUser = 'root',
-    sqHost = 'localhost'
+    sqHost = 'localhost',
+    sqPort = '1983'
 ):
     # <--------------------MySQL connection setup-------------------->
     import mysql.connector as sq
@@ -23,7 +19,7 @@ def CreateSnowflakeDBandSchema (
           user=sqUser,
           password=sqPswd,
           host=sqHost,
-          port="1983"
+          port=sqPort
       )
       sqq = sqConnection.cursor(buffered=True)
       sqq.close()
@@ -41,23 +37,16 @@ def CreateSnowflakeDBandSchema (
     sqq = sqConnection.cursor(buffered=True)
     print("Connected to MySQL")
 
-    # <--------------------Command Execution-------------------->
-    import pandas as pd
-    from snowflake.connector.pandas_tools import write_pandas
-    # Getting table company_dim
-    sqq.execute("USE qmscore")
-    sqq.execute("SELECT * FROM company_dim LIMIT 1000")
-    table_rows = sqq.fetchall()
-    df = pd.DataFrame(table_rows)
-    print("Obtained rows from MySQL and converted into Pandas dataframe")
-    print(df)
-    #print(output)
-    #import csv
-    #with open("out.csv", "w", newline='') as csv_file: 
-    #        csv_writer = csv.writer(csv_file)
-    #        csv_writer.writerow([i[0] for i in sqq.description]) # write headers
-    #        csv_writer.writerows(sqq)
+    return (sqConnection, sqq)
 
+
+def snowflakeConnector (
+    sfPswd = '',
+    sfUser = 'my_user',
+    sfDatabase = 'DEMO_DB',
+    sfSchema = 'DEMO_SCHEMA',
+    sfAccount = ''
+):
     # <--------------------Snowflake connection setup-------------------->
     import snowflake.connector as sf
     # Request user password if not provided already
@@ -85,37 +74,45 @@ def CreateSnowflakeDBandSchema (
     sfq = sfConnection.cursor()
     print("Connected to Snowflake")
 
-    # <--------------------Command Execution-------------------->
+    return (sfConnection, sfq)
+    
+
+def dataLoadFromMySQLtoSnowflake (
+    sfPswd = '',
+    sqPswd = '',
+    sfAccount = 'myAccount.my-region',
+    sfUser = 'my_user',
+    sfDatabase = 'DEMO_DB',
+    sfSchema = 'DEMO_SCHEMA',
+    sqUser = 'root',
+    sqHost = 'localhost',
+    sqPort = '1983'
+):
+    # Connect to MySQL
+    sqConnection, sqq = mysqlConnector (
+        sqPswd = sqPswd,
+        sqUser = sqUser,
+        sqHost = sqHost,
+        sqPort = sqPort
+    )
+    # Connect to Snowflake
+    sfConnection, sfq = snowflakeConnector (
+        sfPswd = sfPswd,
+        sfUser = sfUser,
+        sfDatabase = sfDatabase,
+        sfSchema = sfSchema,
+        sfAccount = sfAccount
+    )
+    import pandas as pd
+    from datetime import datetime 
+    from snowflake.connector.pandas_tools import write_pandas
     from snowflake.sqlalchemy import URL
     from sqlalchemy import create_engine
+    # Setting up Snowflake to load into
     sfq.execute("USE ROLE DEMO_ACCESS")
     sfq.execute("USE WAREHOUSE COMPUTE_WH")
     sfq.execute("USE DATABASE DEMO_DB")
     sfq.execute("USE SCHEMA DEMO_DB.TESTANALYTICS")
-    query = """CREATE OR REPLACE TABLE company_dim (
-  company_seq_id smallint NOT NULL,
-  company_name varchar(250) DEFAULT NULL,
-  company_cik int DEFAULT NULL,
-  company_primary_symbol varchar(20) DEFAULT NULL,
-  company_short_name varchar(100) DEFAULT NULL,
-  company_long_name varchar(250) DEFAULT NULL,
-  company_category_code varchar(5) DEFAULT NULL,
-  company_category varchar(50) DEFAULT NULL,
-  country_seq_id smallint DEFAULT NULL,
-  company_ind_class_system varchar(200) DEFAULT NULL,
-  company_sic varchar(300) DEFAULT NULL,
-  company_sector_type varchar(20) DEFAULT NULL,
-  company_sector varchar(50) DEFAULT NULL,
-  company_industry varchar(100) DEFAULT NULL,
-  company_sub_industry varchar(100) DEFAULT NULL,
-  last_fiscal_year_end date DEFAULT NULL,
-  fiscal_adjustment tinyint DEFAULT NULL,
-  quarter_adjustment tinyint DEFAULT NULL,
-  reporting_currency varchar DEFAULT NULL,
-  PRIMARY KEY (company_seq_id) )
-    """
-    #sfq.execute(query)
-    #success, nchunks, nrows, _ = write_pandas(conn=sfConnection,df=df,table_name='company_dim')
     engine = create_engine(URL(
         account = sfAccount,
         user = sfUser,
@@ -126,9 +123,25 @@ def CreateSnowflakeDBandSchema (
         role='DEMO_ACCESS',
     ))
     engineConnection = engine.connect()
-    print("Engine created")
-    df.to_sql('company_dim', con=engine, index=False)
-    print("Dataframe loaded into Snowflake")
+    # Getting data from MySQL and loading into Snowflake
+    sqq.execute("USE qm_score_prod")
+    table_name = ""
+    print("Enter table to be loaded (enter 'stop' to exit)")
+    while True:
+        table_name = input("Table Name: ")
+        if (table_name=='stop'):
+            break
+        sqq.execute(f"SELECT * FROM {table_name} LIMIT 1000")
+        table_rows = sqq.fetchall()
+        table_headers = sqq.column_names
+        df = pd.DataFrame(table_rows,columns=table_headers)
+        #df = pd.DataFrame(table_rows)
+        begin_time = datetime.now()
+        df.to_sql(table_name, con=engine, index=False)
+        end_time = datetime.now()
+        print(f"{table_name} loaded into Snowflake in {end_time-begin_time}")
+
+    
     engineConnection.close()
     engine.dispose()
     print('Steps complete')
@@ -137,9 +150,9 @@ def CreateSnowflakeDBandSchema (
     sfq.close()
     sfConnection.close()
 
-CreateSnowflakeDBandSchema (
-    sfPswd = '',
-    sqPswd = '',
+dataLoadFromMySQLtoSnowflake (
+    sfPswd = 'Dpa@1234',
+    sqPswd = 'qmreadonly2020',
     sfAccount = 'bqa34388',
     sfUser = 'USER_1',
     sfDatabase = 'DEMO_DB',
@@ -147,3 +160,4 @@ CreateSnowflakeDBandSchema (
     sqUser = 'qm_readonly',
     sqHost = 'qmanalyticsdb.cr4wvcewigkc.us-west-2.rds.amazonaws.com'
 )
+
